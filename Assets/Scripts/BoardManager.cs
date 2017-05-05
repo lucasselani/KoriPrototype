c8b7dac1220e89bbe776e634547d66eabdf64079
Lucas Selani <lucas_selani@hotmail.com>
2017-05-05 17:05:18 -0300

    initial project

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour {
+
+	[Serializable]
+	public class Count{
+		public int minimum;
+		public int maximum;
+
+		public Count(int min, int max){
+			minimum = min;
+			maximum = max;
+		}			
+	}
+
+	public int columns = 64;
+	public int rows = 64;
+	public Count blockingCount = new Count(20,25);
+	public Count objectCount = new Count(5,9); 
+	public Count monsterCount = new Count(4,7);
+	public GameObject[] wallTiles;
+	public GameObject[] floorTiles;
+	public GameObject[] blockingTiles;
+	public GameObject[] monsterTiles;
+	public GameObject[] objectTiles;
+
+	private Transform boardHolder;
+	private List<Vector3> gridPositions = new List<Vector3>();
+
+	void InitializeList(){
+		gridPositions.Clear ();
+		for (int x=1; x<columns-1; x++) {
+			for (int y = 1; y < rows - 1; y++) {
+				gridPositions.Add (new Vector3 (x, y, 0f));
+			}
+		}
+	}
+
+	void BoardSetup(){
+		boardHolder = new GameObject ("Board").transform;
+		for (int x = -1; x < columns + 1; x++) {
+			for (int y = -1; y < rows + 1; y++) {
+				GameObject toInstanciate = floorTiles[Random.Range(0,floorTiles.Length)];
+					if(x==-1 || y==-1 || x==columns || y==rows) toInstanciate = wallTiles[Random.Range(0,wallTiles.Length)];
+				GameObject instace = Instantiate(toInstanciate, new Vector3(x,y,0f), Quaternion.identity) as GameObject;
+				instace.transform.SetParent(boardHolder);
+			}
+		}		
+	}
+
+	Vector3 RandomPosition(){
+		int randomIndex = Random.Range(0, gridPositions.Count);
+		Vector3 RandomPosition = gridPositions[randomIndex];
+		gridPositions.RemoveAt(randomIndex);
+		return RandomPosition;
+	}
+
+	void LayoutObjectAtRandom(GameObject[] tileArray, int min, int max){
+		int objectCount = Random.Range(min, max+1);
+		for(int i=0; i<objectCount; i++){
+			Vector3 randomPosition = RandomPosition();
+			GameObject tileChoice = tileArray[Random.Range(0,tileArray.Length)];
+			Instantiate(tileChoice, randomPosition, Quaternion.identity);
+		}
+	}
+
+	public void SetupScene(){
+		BoardSetup(); //Seta walls de borda e tiles
+		InitializeList(); //Inicializa grid com posições do meio
+		LayoutObjectAtRandom(blockingTiles, blockingCount.minimum, blockingCount.maximum);
+		LayoutObjectAtRandom(monsterTiles, monsterCount.minimum, monsterCount.maximum);
+		LayoutObjectAtRandom(objectTiles, objectCount.minimum, objectCount.maximum);
+	}
+		
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveTime = 5f;
+	public LayerMask BlockingLayer;
+	public GameObject Projectile;
+
+    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+	private BoxCollider2D boxCollider;
+	private Animator animator;
+	private float inverseMoveTime;
+	private float projectileSpeed = 3f;
+	private float fireCooldown = 2f;
+	private float attackCooldown = 0.5f;
+	private float nextFire = 0;
+	private float nextAttack = 0;
+
+	// Use this for initialization
+	void Start () {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+		boxCollider = GetComponent<BoxCollider2D>();
+		rb2d = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+		inverseMoveTime = 1f/moveTime;
+			
+	}
+
+	private bool Move(int xDir, int yDir, out RaycastHit2D hit){ 
+		 Vector2 start = this.transform.position;
+         Vector2 end = start +  new Vector2(xDir,yDir) * this.inverseMoveTime * Time.deltaTime;
+
+		 boxCollider.enabled = false;
+         hit = Physics2D.Linecast (start, end, BlockingLayer);
+		 boxCollider.enabled = true;
+ 
+         if(hit.transform == null){		
+			 StartCoroutine(SmoothMovement(end));	 
+			 return true;
+		 } else	return false;		 
+	}
+
+	protected IEnumerator SmoothMovement(Vector3 end){
+		Vector3 newPosition = new Vector3(end.x, end.y, 0f);
+        rb2d.MovePosition (newPosition);
+		yield return null;
+	}
+
+	private void AttemptMove(){
+		int horizontal = (int) Input.GetAxisRaw("Horizontal");
+		int vertical = (int) Input.GetAxisRaw("Vertical");
+
+		if(horizontal != 0 || vertical != 0){
+			animator.SetTrigger("walk");
+		} else{
+			animator.SetTrigger("idle");
+			return;
+		} 
+		RaycastHit2D hit;
+		bool canMove = Move(horizontal, vertical, out hit);
+		if(!canMove) CheckCollision(hit);
+	}
+
+	private void CheckCollision(RaycastHit2D hit){
+		if(hit.collider.tag == "Block") Debug.Log("Block");
+	}
+
+	void Attack(){
+		if (Input.GetButton("Fire2") && Time.time > nextFire){
+			nextFire = Time.time + fireCooldown;
+
+			Vector3 mouse = Input.mousePosition;
+			mouse.z = 0.0f;
+			mouse = Camera.main.ScreenToWorldPoint(mouse);
+			mouse = mouse-transform.position;
+
+			GameObject projectile = Instantiate(Projectile, transform.position, Quaternion.identity);
+			projectile.GetComponent<Rigidbody2D>().velocity = (new Vector3(mouse.x*projectileSpeed,mouse.y*projectileSpeed,0f));
+			Destroy(projectile.gameObject,3);
+		} else if(Input.GetButton("Fire1") && Time.time > nextAttack){
+			nextAttack = Time.time + attackCooldown;
+
+			Vector3 mouse = Input.mousePosition;
+			mouse.z = 0.0f;
+			mouse = Camera.main.ScreenToWorldPoint(mouse);
+			mouse = mouse-transform.position;	
+			StartCoroutine(AttackSmooth());		
+		}
+	}
+
+	IEnumerator AttackSmooth(){
+		animator.SetTrigger("attack");
+		yield return new WaitForSeconds(2f);
+	}
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {	
+		AttemptMove();
+		Attack();
+	}
+}
